b8b65925e945e5d08f9a9204d43d1819907478a8
Jason Huang <jason97521@gmail.com>
2023-04-12 04:32:21 -0400

    count functionality from db

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express"
 import Hotel from "../models/Hotel.js"
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js"
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js"
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js"
 
 const router = express.Router()
@@ -15,9 +15,11 @@ router.put("/:id", verifyAdmin, updateHotel)
 router.delete("/:id", verifyAdmin, deleteHotel)
 
 //Get
-router.get("/:id", getHotel)
+router.get("/find/:id", getHotel)
 
 //Get All
 router.get("/", getHotels)
+router.get("/countByCity", countByCity)
+router.get("/countByType", getHotels)
 
 export default router
