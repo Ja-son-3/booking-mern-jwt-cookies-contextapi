53d6701e1b047e513dd9ea58eb83bd1208c52f74
Jason Huang <jason97521@gmail.com>
2023-04-12 14:38:08 -0400

    Admin Crud operations

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -9,8 +9,7 @@ export const register = async (req, res, next) => {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
         const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
+            ...req.body,
             password: hash,
         })
 
@@ -33,7 +32,7 @@ export const login = async (req, res, next) => {
         const {password, isAdmin, ...otherDetails} = user._doc
         res.cookie("access_token", token, {
             httpOnly: true,
-        }).status(200).send({...otherDetails})
+        }).status(200).send({details:{...otherDetails}, isAdmin})
     } catch (err) {
         next(err)
     }
