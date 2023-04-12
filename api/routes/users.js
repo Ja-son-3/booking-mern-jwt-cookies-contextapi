4477da2b915b3762dfa045fb4b210f19375afdfd
Jason Huang <jason97521@gmail.com>
2023-04-11 22:07:06 -0400

    jwt, user crud

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,18 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js"
 
 const router = express.Router()
 
+//Update
+router.put("/:id", updateUser)
+
+//Delete
+router.delete("/:id", deleteUser)
+
+//Get
+router.get("/:id", getUser)
+
+//Get All
+router.get("/", getUsers)
 
 export default router
