76caecd2fd019238f73c5ca6fdad23452103a1fb
Jason Huang <jason97521@gmail.com>
2023-04-11 22:39:13 -0400

    jwt role-based authorization verification

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
-app.use("/api/user", usersRoute)
+app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
 app.use("/api/rooms", roomsRoute)
 
